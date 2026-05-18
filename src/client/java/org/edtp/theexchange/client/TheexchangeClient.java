4569e692a8288081358cd73805a5c404d18b1d6c
GBwater <gbwater@outlook.com>
2026-05-18 18:52:37 +0800

    init

---
+++ src/client/java/org/edtp/theexchange/client/TheexchangeClient.java
@@ -0,0 +1,10 @@
+package org.edtp.theexchange.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheexchangeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/edtp/theexchange/Theexchange.java
@@ -0,0 +1,10 @@
+package org.edtp.theexchange;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Theexchange implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
